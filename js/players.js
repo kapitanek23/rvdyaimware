3dbddac097621714a7d3db24386216957c1ae487
Bartosz Kawalec <bartekka2003@gmail.com>
2026-02-11 20:42:10 +0100

    Podstawowe strony zawodników

---
+++ js/players.js
@@ -0,0 +1,63 @@
+const playersData = {
+    "rvdy": {
+        nick: "rvdy",
+        role: "Entry Fragger",
+        image: "assets/img/rvdy.jpg", // Upewnij się, że masz folder assets/img
+        stats: {
+            rating: "1.15",
+            kd: "1.12",
+            hs: "45%"
+        },
+        desc: "Trafia tylko jak nikt nie patrzy.",
+        // Pamiętaj: Link musi być w formacie embed, nie watch!
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    },
+    "legit": {
+        nick: "legit",
+        role: "Sniper",
+        image: "assets/img/legit.jpg",
+        stats: {
+            rating: "1.05",
+            kd: "1.08",
+            hs: "32%"
+        },
+        desc: "Lubi drzeć ryja na Eco, ale strzela głowy.",
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    },
+    "dawidek": {
+        nick: "Dawidek2012PL",
+        role: "Rifler",
+        image: "assets/img/dawidek.jpg",
+        stats: {
+            rating: "0.98",
+            kd: "0.95",
+            hs: "40%"
+        },
+        desc: "Musi kończyć o 22:00, bo rano przyroda. Ale głowy strzela jak profesor.",
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    },
+    "saju": {
+        nick: "Saju",
+        role: "Support",
+        image: "assets/img/saju.jpg",
+        stats: {
+            rating: "1.00",
+            kd: "0.99",
+            hs: "35%"
+        },
+        desc: "Trafia niesamowite flickshoty, ale nie potrafi trafić w plecy stojącego przeciwnika.",
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    },
+    "bibi": {
+        nick: "bibi",
+        role: "Lurker",
+        image: "assets/img/bibi.jpg",
+        stats: {
+            rating: "1.10",
+            kd: "1.05",
+            hs: "50%"
+        },
+        desc: "Król granatów i flashy na ryj.",
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    }
+};
